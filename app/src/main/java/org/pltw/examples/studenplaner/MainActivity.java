5133395a704b4c1841290776be3a90a0376f7ac8
Baandhav <47670334+Baandhav@users.noreply.github.com>
2019-03-22 11:01:33 -0500

    Added Popup menu

--- app/src/main/java/org/pltw/examples/studenplaner/MainActivity.java
+++ app/src/main/java/org/pltw/examples/studenplaner/MainActivity.java
@@ -5,11 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-private Button mCButton; // Create assignment button
-private Button mDButton; // Delete assignment button
-private Button mSButton; // Share button
+    private Button mCButton; // Create assignment button
+    private Button mDButton; // Delete assignment button
+    private Button mSButton; // Share button
+    private ImageView popup;
+    private EditText mCreateName;
+    private EditText mCreateType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +22,22 @@ private Button mSButton; // Share button
         mCButton = (Button) findViewById(R.id.cButton);
         mDButton = (Button) findViewById(R.id.dButton);
         mSButton = (Button) findViewById(R.id.sButton);
+        popup = (ImageView) findViewById(R.id.createPopup);
+        mCreateName = (EditText) findViewById(R.id.createName);
+        mCreateType = (EditText) findViewById(R.id.createType);
     }
-    public void onClickCreate(View v){
-        startActivity(new Intent(MainActivity.this,Pop.class))
+
+    public void onClickCreate(View v) {
+        if (popup.getVisibility() == View.VISIBLE) {
+
+            popup.setVisibility(View.INVISIBLE);
+            mCreateType.setVisibility(View.INVISIBLE);
+            mCreateName.setVisibility(View.INVISIBLE);
+        }
+        else {
+            popup.setVisibility(View.VISIBLE);
+            mCreateType.setVisibility(View.VISIBLE);
+            mCreateName.setVisibility(View.VISIBLE);
+        }
     }
 }
-
