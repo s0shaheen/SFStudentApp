6a46667539dbe0e340cb0dc996a7fdf32e84363b
Baandhav <47670334+Baandhav@users.noreply.github.com>
2019-04-05 11:02:48 -0500

    Working

--- app/src/main/java/org/pltw/examples/studenplaner/Assignment.java
+++ app/src/main/java/org/pltw/examples/studenplaner/Assignment.java
@@ -1,69 +1,49 @@
 package org.pltw.examples.studenplaner;
 
 public class Assignment {
-    private int hours; // The hour value of when the assignment was created.
-    private int minutes; // The minute value of when the assignment was created.
     private String name; // Name of the assignment.
-    private int month; // Month of the due date of the assignment.
-    private int day; // Day of the due date of the assignment.
+    private String month; // Month of the due date of the assignment.
+    private String day; // Day of the due date of the assignment.
     private String type; // Type of assignment.
 public Assignment(){
-    hours = 0;
-    minutes = 0;
     name = "";
-    month = 0;
-    day = 0;
+    month = "";
+    day = "";
     type = "";
 }
-public Assignment(int hours, int minutes, String name, int month, int day, String type){
-    this.hours = hours;
-    this.minutes = minutes;
+public Assignment(String name, String month, String day, String type){
     this.name = name;
     this.month = month;
     this.day = day;
     this.type = type;
 }
 //Getters
-    public int getHours() {
-        return hours;
-    }
-
-    public int getMinutes() {
-        return minutes;
-    }
 
     public String getName() {
         return name;
     }
 
-    public int getMonth() {
+    public String getMonth() {
         return month;
     }
 
     public String getType() {
         return type;
     }
-    public int getDay() {
+    public String getDay() {
         return day;
     }
 //Setters
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
-
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
-    }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setMonth(int month) {
+    public void setMonth(String month) {
         this.month = month;
     }
 
-    public void setDay(int day) {
+    public void setDay(String day) {
         this.day = day;
     }
 
--- app/src/main/java/org/pltw/examples/studenplaner/MainActivity.java
+++ app/src/main/java/org/pltw/examples/studenplaner/MainActivity.java
@@ -8,13 +8,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     private Button mCButton; // Create assignment button
     private Button mDButton; // Delete assignment button
     private Button mSButton; // Share button
+    private Button mAssignemnt;
+    private Button mCreateSubmit;
+    private Button close;
     private ImageView popup;
+    private ImageView aView;
     private EditText mCreateName;
     private EditText mCreateType;
+    private EditText mCreateMonth;
+    private EditText mCreateDay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +30,15 @@ public class MainActivity extends AppCompatActivity {
         mCButton = (Button) findViewById(R.id.cButton);
         mDButton = (Button) findViewById(R.id.dButton);
         mSButton = (Button) findViewById(R.id.sButton);
+        mCreateSubmit = (Button) findViewById(R.id.submit);
+        mAssignemnt = (Button) findViewById(R.id.Assignment);
+        close = (Button) findViewById(R.id.closeButton);
         popup = (ImageView) findViewById(R.id.createPopup);
+        aView = (ImageView) findViewById(R.id.aMenu);
         mCreateName = (EditText) findViewById(R.id.createName);
         mCreateType = (EditText) findViewById(R.id.createType);
+        mCreateMonth = (EditText) findViewById(R.id.month);
+        mCreateDay = (EditText) findViewById(R.id.day);
     }
 
     public void onClickCreate(View v) {
@@ -33,11 +47,40 @@ public class MainActivity extends AppCompatActivity {
             popup.setVisibility(View.INVISIBLE);
             mCreateType.setVisibility(View.INVISIBLE);
             mCreateName.setVisibility(View.INVISIBLE);
+            mCreateMonth.setVisibility(View.INVISIBLE);
+            mCreateDay.setVisibility(View.INVISIBLE);
+            mCreateSubmit.setVisibility(View.INVISIBLE);
         }
         else {
             popup.setVisibility(View.VISIBLE);
             mCreateType.setVisibility(View.VISIBLE);
             mCreateName.setVisibility(View.VISIBLE);
+            mCreateMonth.setVisibility(View.VISIBLE);
+            mCreateDay.setVisibility(View.VISIBLE);
+            mCreateSubmit.setVisibility(View.VISIBLE);
         }
+    }
+    public void onClickSubmit(View v){
+        mAssignemnt.setVisibility(View.VISIBLE);
+        popup.setVisibility(View.INVISIBLE);
+        mCreateType.setVisibility(View.INVISIBLE);
+        mCreateName.setVisibility(View.INVISIBLE);
+        mCreateMonth.setVisibility(View.INVISIBLE);
+        mCreateDay.setVisibility(View.INVISIBLE);
+        mCreateSubmit.setVisibility(View.INVISIBLE);
+    }
+    public void onClickAssignemnt(View v){
+        aView.setVisibility((View.VISIBLE));
+        mCreateType.setVisibility(View.VISIBLE);
+        mCreateName.setVisibility(View.VISIBLE);
+        mCreateMonth.setVisibility(View.VISIBLE);
+        mCreateDay.setVisibility(View.VISIBLE);
+    }
+    public void onClickClose(View v){
+        aView.setVisibility(View.INVISIBLE);
+        mCreateType.setVisibility(View.INVISIBLE);
+        mCreateName.setVisibility(View.INVISIBLE);
+        mCreateMonth.setVisibility(View.INVISIBLE);
+        mCreateDay.setVisibility(View.INVISIBLE);
     }
 }
